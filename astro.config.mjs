804d1328fec63f18fffa2d3cc99bc65fdcccbda9
Eleni Papanicolas <elenipapanicolas@gmail.com>
2024-02-03 19:30:01 -0500

    fix URL in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,15 @@ import { defineConfig, squooshImageService } from "astro/config";
 import robots from "astro-robots";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-// import netlify from "@astrojs/netlify";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "ubiquitous-madeleine-e24808.netlify.app",
+  site: 'https://ubiquitous-madeleine-e24808.netlify.app',
   image: {
     service: squooshImageService(),
   },
   integrations: [tailwind(), sitemap(), robots()],
-  // output: "server",
-  // adapter: netlify(),
+  output: "server",
+  adapter: netlify(),
 });
