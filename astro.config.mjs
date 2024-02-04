cbeab7db3f16c60528028181c01da34fc9bc4ba9
Eleni Papanicolas <elenipapanicolas@gmail.com>
2024-02-03 19:23:45 -0500

    add deployed site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "ubiquitous-madeleine-e24808.netlify.app",
   image: {
     service: squooshImageService(),
   },
